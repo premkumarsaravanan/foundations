a5ffd10070a43ec7daa2700cea6e12e67a77ad4a
vuhuucuong <vuhuucuong1310@gmail.com>
2020-02-20 14:56:38 +0700

    chore: temporary disalbe build:prod for graphql-server

--- packages/graphql-server/src/resolvers/index.ts
+++ packages/graphql-server/src/resolvers/index.ts
@@ -6,67 +6,24 @@ import {
   queryIdentityCheckById,
   updateIdentityCheck,
 } from './identity-check/resolvers'
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { queryArea, queryAreas, mutationCreateArea, mutationUpdateArea } from './area/resolvers'
-=======
-=======
-import { createContactIdentityCheck } from './contact-identity-check/resolvers'
-import { queryNegotiator, queryNegotiators, createNegotiator, updateNegotiator } from './negotiators/resolvers'
-<<<<<<< HEAD
->>>>>>> feat: #371 Implement resolver for graphql server: negotiator services
->>>>>>> feat: #371 Implement resolver for graphql server: negotiator services
-=======
->>>>>>> feat: #371 Fix comments
-=======
->>>>>>> feat: #371 Implement resolver for graphql server: negotiator services
 import { login } from './auth/resolvers'
-import { queryNegotiatorById, queryNegotiators, createNegotiator, updateNegotiator } from './negotiators/resolvers'
 
 export const resolvers = {
   Query: {
     contact: queryContact,
     contacts: queryContacts,
-<<<<<<< HEAD
     GetIdCheckById: queryIdentityCheckById,
     GetIdChecks: queryIdentityChecks,
-<<<<<<< HEAD
     area: queryArea,
     areas: queryAreas,
-=======
-    negotiator: queryNegotiatorById,
-    negotiators: queryNegotiators,
->>>>>>> feat: #371 Fix comments
   },
   Mutation: {
     login,
     CreateIdentityCheck: createIdentityCheck,
     UpdateIdentityCheck: updateIdentityCheck,
-<<<<<<< HEAD
-<<<<<<< HEAD
     createArea: mutationCreateArea,
     updateArea: mutationUpdateArea,
-=======
-=======
-=======
->>>>>>> feat: #371 Implement resolver for graphql server: negotiator services
-    negotiator: queryNegotiator,
-    negotiators: queryNegotiators,
-  },
-  Mutation: {
-    login,
-    createContactIdentityCheck,
-    createNegotiator,
-    updateNegotiator,
-<<<<<<< HEAD
->>>>>>> feat: #371 Implement resolver for graphql server: negotiator services
->>>>>>> feat: #371 Implement resolver for graphql server: negotiator services
-=======
-    createNegotiator,
-    updateNegotiator,
->>>>>>> feat: #371 Fix comments
-=======
->>>>>>> feat: #371 Implement resolver for graphql server: negotiator services
   },
   JSON: GraphQLJSON,
 }
