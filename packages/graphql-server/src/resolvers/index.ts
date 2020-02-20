cb0859b46b370d951860dfae1ab7995ceca3eca0
Khac Vy <khacvy93@gmail.com>
2020-02-20 14:12:53 +0700

    feat: #371 Implement resolver for graphql server: negotiator services

--- packages/graphql-server/src/resolvers/index.ts
+++ packages/graphql-server/src/resolvers/index.ts
@@ -1,18 +1,27 @@
 import GraphQLJSON from 'graphql-type-json'
 import { queryContact, queryContacts } from './contact/resolvers'
+<<<<<<< HEAD
 import {
   createIdentityCheck,
   queryIdentityChecks,
   queryIdentityCheckById,
   updateIdentityCheck,
 } from './identity-check/resolvers'
+<<<<<<< HEAD
 import { queryArea, queryAreas, mutationCreateArea, mutationUpdateArea } from './area/resolvers'
+=======
+=======
+import { createContactIdentityCheck } from './contact-identity-check/resolvers'
+import { queryNegotiator, queryNegotiators, createNegotiator, updateNegotiator } from './negotiators/resolvers'
+>>>>>>> feat: #371 Implement resolver for graphql server: negotiator services
+>>>>>>> feat: #371 Implement resolver for graphql server: negotiator services
 import { login } from './auth/resolvers'
 
 export const resolvers = {
   Query: {
     contact: queryContact,
     contacts: queryContacts,
+<<<<<<< HEAD
     GetIdCheckById: queryIdentityCheckById,
     GetIdChecks: queryIdentityChecks,
     area: queryArea,
@@ -22,8 +31,21 @@ export const resolvers = {
     login,
     CreateIdentityCheck: createIdentityCheck,
     UpdateIdentityCheck: updateIdentityCheck,
+<<<<<<< HEAD
     createArea: mutationCreateArea,
     updateArea: mutationUpdateArea,
+=======
+=======
+    negotiator: queryNegotiator,
+    negotiators: queryNegotiators,
+  },
+  Mutation: {
+    login,
+    createContactIdentityCheck,
+    createNegotiator,
+    updateNegotiator,
+>>>>>>> feat: #371 Implement resolver for graphql server: negotiator services
+>>>>>>> feat: #371 Implement resolver for graphql server: negotiator services
   },
   JSON: GraphQLJSON,
 }
