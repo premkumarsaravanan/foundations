f3780699eb436a94f55bf6f56b78e6eda6493fa1
Khac Vy <khacvy93@gmail.com>
2020-02-21 13:58:58 +0700

    feat: #371 Fix comments

--- packages/graphql-server/src/resolvers/index.ts
+++ packages/graphql-server/src/resolvers/index.ts
@@ -1,12 +1,12 @@
 import GraphQLJSON from 'graphql-type-json'
 import { queryContact, queryContacts } from './contact/resolvers'
-<<<<<<< HEAD
 import {
   createIdentityCheck,
   queryIdentityChecks,
   queryIdentityCheckById,
   updateIdentityCheck,
 } from './identity-check/resolvers'
+<<<<<<< HEAD
 <<<<<<< HEAD
 import { queryArea, queryAreas, mutationCreateArea, mutationUpdateArea } from './area/resolvers'
 =======
@@ -15,22 +15,30 @@ import { createContactIdentityCheck } from './contact-identity-check/resolvers'
 import { queryNegotiator, queryNegotiators, createNegotiator, updateNegotiator } from './negotiators/resolvers'
 >>>>>>> feat: #371 Implement resolver for graphql server: negotiator services
 >>>>>>> feat: #371 Implement resolver for graphql server: negotiator services
+=======
+>>>>>>> feat: #371 Fix comments
 import { login } from './auth/resolvers'
+import { queryNegotiatorById, queryNegotiators, createNegotiator, updateNegotiator } from './negotiators/resolvers'
 
 export const resolvers = {
   Query: {
     contact: queryContact,
     contacts: queryContacts,
-<<<<<<< HEAD
     GetIdCheckById: queryIdentityCheckById,
     GetIdChecks: queryIdentityChecks,
+<<<<<<< HEAD
     area: queryArea,
     areas: queryAreas,
+=======
+    negotiator: queryNegotiatorById,
+    negotiators: queryNegotiators,
+>>>>>>> feat: #371 Fix comments
   },
   Mutation: {
     login,
     CreateIdentityCheck: createIdentityCheck,
     UpdateIdentityCheck: updateIdentityCheck,
+<<<<<<< HEAD
 <<<<<<< HEAD
     createArea: mutationCreateArea,
     updateArea: mutationUpdateArea,
@@ -46,6 +54,10 @@ export const resolvers = {
     updateNegotiator,
 >>>>>>> feat: #371 Implement resolver for graphql server: negotiator services
 >>>>>>> feat: #371 Implement resolver for graphql server: negotiator services
+=======
+    createNegotiator,
+    updateNegotiator,
+>>>>>>> feat: #371 Fix comments
   },
   JSON: GraphQLJSON,
 }
